d048c70d255b5547ae00ff2a7ab007638c224119
hnmaruf19 <marufmaruf09112002@gmail.com>
2025-09-21 16:09:16 +0600

    Add new file

--- lab1.cpp
+++ lab1.cpp
@@ -5,6 +5,5 @@ typedef long long ll;
 int main() {
     cout<<"My first line written in C++"<<endl;
     cout<<"This is my second line written in C++ and it is being tracked by git"<<endl;
-    cout<<"The pro coder, the developer: Ahsanul Habib"<<endl;
     return 0;
 }
