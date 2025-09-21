9c4ab3913eb8498932c9a61e33ab544f4a02f8fe
hnmaruf19 <marufmaruf09112002@gmail.com>
2025-09-21 16:03:09 +0600

    Add some appreciation

--- lab1.cpp
+++ lab1.cpp
@@ -5,5 +5,6 @@ typedef long long ll;
 int main() {
     cout<<"My first line written in C++"<<endl;
     cout<<"This is my second line written in C++ and it is being tracked by git"<<endl;
+    cout<<"The pro coder, the developer: Ahsanul Habib"<<endl;
     return 0;
 }
